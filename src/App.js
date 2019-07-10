17c630b7e09b9617af01b4e7e10df2d9c6d920ec
doohong <wnghd94@gmail.com>
2019-07-10 23:30:35 +0900

    movieApp start

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react'; 
 import { BrowserRouter as Router, Route } from 'react-router-dom'; 
 import './App.css'; 
+import Movie from './Movie';
 import Main from "./container/Main";
 
 
 class App extends Component {
   render(){
     return (
-      <Router> 
-        <div> 
-          <Route exact path="/" component={Main} /> 
-        </div> 
-      </Router>
-
+      <div className="App">
+        <Movie />
+      </div>
     );
   }
 }
