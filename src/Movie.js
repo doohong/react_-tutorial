45b17db54805dbb52354168de676cded133f1883
doohong <wnghd94@gmail.com>
2019-07-14 16:19:34 +0900

    Smart vs Dumb

--- src/Movie.js
+++ src/Movie.js
@@ -2,30 +2,27 @@ import React, {Component} from 'react';
 import './Movie.css';
 import PropTypes from 'prop-types';
 
-class Movie extends Component{
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        poster: PropTypes.string.isRequired
-    }
-    render(){
-        return(
-            <div>
-                <MoviePoster poster={this.props.poster} />
-                <h1>{this.props.title}</h1>
-            </div>
-        );
-    }
+
+
+function Movie({poster,title}){
+    return(
+        <div>
+            <MoviePoster poster={poster} />
+            <h1>{title}</h1>
+        </div>
+    );
 }
 
-class MoviePoster extends Component{
-    propTypes={
-        poster: PropTypes.string
-    }
-    render(){
-        return(
-            <img src={this.props.poster} />
-        );
-    }
+function MoviePoster({poster}){
+    return(
+        <img src={poster} />
+    );
 }
-
+Movie.propTypes={
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired
+}
+MoviePoster.propTypes={
+    poster: PropTypes.string.isRequired
+}
 export default Movie;
