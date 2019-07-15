980c5163bbf22d0b7549c9cd84626f0e16bd5056
doohong <wnghd94@gmail.com>
2019-07-15 21:31:40 +0900

    AJAX on React

--- src/component/Gnb.js
+++ src/component/Gnb.js
@@ -3,13 +3,13 @@ import React from 'react';
 const Gnb = () =>(
     <ul className="gnb">
         <li>
-            <a href="#none">메뉴</a>
+            <a href="#none" className="tab">메뉴</a>
         </li>
         <li>
-            <a href="#none">메뉴</a>
+            <a href="#none" className="tab">메뉴</a>
         </li>
         <li>
-            <a href="#none">메뉴</a>
+            <a href="#none" className="tab">메뉴</a>
         </li>
     </ul>
 )
