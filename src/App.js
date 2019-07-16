4e2f293d2abf93c4fc3b41f7c15bb940f831f8bd
doohong <wnghd94@gmail.com>
2019-07-16 23:19:34 +0900

    ajax

--- src/App.js
+++ src/App.js
@@ -6,29 +6,7 @@ class App extends Component {
   state={
   };
   componentDidMount(){
-    setTimeout(()=>{
-      this.setState({
-        movies:[
-          { 
-            title: "스파이더맨: 파 프롬 홈",
-            poster: "http://img.cgv.co.kr/Movie/Thumbnail/Poster/000081/81619/81619_1000.jpg"
-          },
-          {
-            title: "알라딘",
-            poster: "https://t1.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/5uYQ/image/c94Iw38EZ8ECL1BlhPJI2s9kfx4.jpg"
-          },
-          {
-            title: "토이스토리4",
-            poster: "https://pbs.twimg.com/media/D2t2_k0WsAA1hdh.jpg"
-          },
-          {
-            title: "기생충",
-            poster: "https://upload.wikimedia.org/wikipedia/ko/thumb/6/60/%EA%B8%B0%EC%83%9D%EC%B6%A9_%ED%8F%AC%EC%8A%A4%ED%84%B0.jpg/250px-%EA%B8%B0%EC%83%9D%EC%B6%A9_%ED%8F%AC%EC%8A%A4%ED%84%B0.jpg"
-          }
-        ]
-      });
-      
-    },5000)
+    fetch('https://yts.lt/api/v2/list_movies.json?sort_by=rating')
   }
 _renderMovies = () => {
   const movies =  this.state.movies.map((movie,index) => {
