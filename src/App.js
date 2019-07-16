bd62bdf82d0bbb8fe386caf7739ededaf78cf401
doohong <wnghd94@gmail.com>
2019-07-16 23:28:54 +0900

    Promises

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ class App extends Component {
   };
   componentDidMount(){
     fetch('https://yts.lt/api/v2/list_movies.json?sort_by=rating')
+    .then(response => response.json())
+    .then(json => console.log(json)) 
+    .catch(err => console.log(err))
+
   }
 _renderMovies = () => {
   const movies =  this.state.movies.map((movie,index) => {
