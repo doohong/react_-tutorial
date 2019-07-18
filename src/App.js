233929d22a7c51d5f4eff543a3f9a9390da166c4
doohong <wnghd94@gmail.com>
2019-07-18 22:51:08 +0900

    Async Await

--- src/App.js
+++ src/App.js
@@ -3,21 +3,28 @@ import React, { Component } from 'react';
 import Movie from './Movie';
 
 class App extends Component {
-  state={
-  };
+  state={}
   componentDidMount(){
-    fetch('https://yts.lt/api/v2/list_movies.json?sort_by=rating')
-    .then(response => response.json())
-    .then(json => console.log(json)) 
-    .catch(err => console.log(err))
-
+    this._getMovies()
   }
-_renderMovies = () => {
-  const movies =  this.state.movies.map((movie,index) => {
-    return <Movie title={movie.title} poster={movie.poster} key={index} />
-  });
-  return movies;
-};
+  _renderMovies = () => {
+    const movies =  this.state.movies.map((movie,index) => {
+      return <Movie title={movie.title} poster={movie.poster} key={index} />
+    })
+    return movies
+  }
+  async _getMovies=() => {
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    })
+  }
+  _callApi = () => {
+    return fetch('https://yts.lt/api/v2/list_movies.json?sort_by=rating')
+    .then(response => response.json())
+    .then(json => json.data.movies) 
+    .catch(err => console.log(err))
+  }
   render(){
     return (
       <div className="App">
