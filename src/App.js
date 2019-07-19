c82ace2535c64467449b075323744052f77336f7
doohong <wnghd94@gmail.com>
2019-07-19 23:34:39 +0900

    Async Await

--- src/App.js
+++ src/App.js
@@ -9,20 +9,25 @@ class App extends Component {
   }
   _renderMovies = () => {
     const movies =  this.state.movies.map((movie,index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index} />
+      return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id} />
     })
     return movies
   }
-  async _getMovies=() => {
+   _getMovies = async () => {
+    try{
     const movies = await this._callApi()
     this.setState({
       movies
     })
+    }catch(er){ 
+      console.log(er)
+    }
   }
   _callApi = () => {
     return fetch('https://yts.lt/api/v2/list_movies.json?sort_by=rating')
     .then(response => response.json())
-    .then(json => json.data.movies) 
+    .then(json => json.data.movies)
+    //.then(movies => console.log(movies)) 
     .catch(err => console.log(err))
   }
   render(){
